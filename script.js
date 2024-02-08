61e2928c0580dd443e6c9e3a3d5c2229f4627bc3
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-02-08 23:55:53 +0545

    nk

---
+++ script.js
@@ -0,0 +1,105 @@
+const main=document.getElementById('sing');
+function animate(){
+    main.innerHTML='';
+    setTimeout(function(){
+        main.innerHTML="P";
+    },200);
+    setTimeout(function(){
+        main.innerHTML="PL";
+    },300)
+    setTimeout(function(){
+        main.innerHTML="PLA";
+    },400)
+    setTimeout(function(){
+        main.innerHTML="PLAY";
+    },500)
+    setTimeout(function(){
+        main.innerHTML="PLAYB";
+    },600)
+    setTimeout(function(){
+        main.innerHTML="PLAYBA";
+    },700)
+    setTimeout(function(){
+        main.innerHTML="PLAYBAC";
+    },800)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK";
+    },900)
+    // setTimeout(function(){
+    //     main.innerHTML=" ";
+    // },900)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK S";
+    },1000)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK SI";
+    },1100)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK SIN";
+    },1200)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK SING";
+    },1300)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK SINGE";
+    },1400)
+    setTimeout(function(){
+        main.innerHTML="PLAYBACK SINGER";
+    },1500)
+    setTimeout(function(){
+        main.innerHTML="P";
+    },2100)
+    setTimeout(function(){
+        main.innerHTML="PE";
+    },2200)
+    setTimeout(function(){
+        main.innerHTML="PER";
+    },2300)
+    setTimeout(function(){
+        main.innerHTML="PERF";
+    },2400)
+    setTimeout(function(){
+        main.innerHTML="PERFO";
+    },2500)
+    setTimeout(function(){
+        main.innerHTML="PERFOR";
+    },2600)
+    setTimeout(function(){
+        main.innerHTML="PERFORM";
+    },2700)
+    setTimeout(function(){
+        main.innerHTML="PERFORME";
+    },2800)
+    setTimeout(function(){
+        main.innerHTML="PERFORMER";
+    },2900)
+    setTimeout(function(){
+        main.innerHTML="M";
+    },3500)
+    setTimeout(function(){
+        main.innerHTML="MU";
+    },3600)
+    setTimeout(function(){
+        main.innerHTML="MUS";
+    },3700)
+    setTimeout(function(){
+        main.innerHTML="MUSI";
+    },3800)
+    setTimeout(function(){
+        main.innerHTML="MUSIC";
+    },3900)
+    setTimeout(function(){
+        main.innerHTML="MUSICI";
+    },4000)
+    setTimeout(function(){
+        main.innerHTML="MUSICIA";
+    },4100)
+    setTimeout(function(){
+        main.innerHTML="MUSICIAN";
+    },4200)
+    setTimeout(function(){
+        animate();
+    },5000)
+
+}
+animate();
